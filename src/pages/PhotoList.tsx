86c070c47eb0707d17946b18a4e210cf0c5ae9a5
Kristy <kprieto14@gmail.com>
2024-02-02 22:48:11 -0500

    Change categoryItem in PhotoList page to be a type of GalleryItemType instead of any.

--- src/pages/PhotoList.tsx
+++ src/pages/PhotoList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import gallery from '../gallery.json'
+import { GalleryItemType } from "../App";
 
 export function PhotoList() {
     const params = useParams<{ category:string }>()
@@ -9,25 +10,32 @@ export function PhotoList() {
     //Turns JSON object into an array to work with
     const entries = Object.entries(gallery)
     
-    //Unfortunately have to set the type as any for now, using GalleryItemType will give errors later when mapping through objects
-    const [ categoryItem, setCategoryItem ] = useState<any>({
+    const [ categoryItem, setCategoryItem ] = useState<GalleryItemType>({
+        category: '',
         title: '',
         description: '',
-        photos: ['']
+        photos: []
     })
 
     useEffect(function() {
-        //Goes through object array
+        //Goes through the object array
         entries.forEach(function(key) {
             //Checks if the key is the category
             if( key.includes(String(params.category)) ) {
                 //Always grab the actual object which is the 2nd in the array
-                var entry = key[1];
-                //Sets the found object
-                setCategoryItem(entry)
+                const entry = key[1];
+                //Manually sets object so that it can be set as a GalleryItemType
+                var foundCategory = {
+                    category: String(params.category),
+                    title: entry.title,
+                    description: entry.description,
+                    photos: entry.photos,
+                }
+
+                setCategoryItem(foundCategory)
             }
         })
-    })
+    }, [])
 
     return (
         <>
