86a85bf0a2ae24c490b9c67a0ddf68eb293d93b1
Kristy <kprieto14@gmail.com>
2024-02-06 21:09:26 -0500

    Fix CSS styling to Photo-Item and also be able to show specific image that is being clicked on for more details.

--- src/pages/PhotoDetail.tsx
+++ src/pages/PhotoDetail.tsx
@@ -1,12 +1,48 @@
-import React from "react"; 
+import React, { useEffect, useState } from "react"; 
 import { useParams } from "react-router-dom";
+import gallery from '../gallery.json'
+import { PhotoItemType } from "../App";
 
 export function PhotoDetail() {
     const params = useParams<{ category:string, title:string }>()
 
-    console.log(params)
+    const [ photo, setPhoto ] = useState<PhotoItemType>({
+        category:'',
+        title:'',
+        imageURL:'',
+        sourceURL:''
+    })
+
+    useEffect(function() {
+        //Turns JSON object into an array to work with
+        const entries = Object.entries(gallery)
+        //Goes through the object array
+        entries.forEach(function(key) {
+            //Always grab the actual object which is the 2nd in the array
+            const entry = key[1];
+            //Checks if the key is the category
+            if( key.includes( String(params.category) )) {
+                //Manually sets object so that it can be set as a PhotoItemType
+                entry.photos.forEach(function(photoObject) {
+                    if(photoObject.title.includes(String(params.title))) {
+                        const foundPhoto = {
+                            category: '',
+                            title: photoObject.title,
+                            imageURL: photoObject.imageURL,
+                            sourceURL: photoObject.sourceURL
+                        }
+
+                        setPhoto(foundPhoto)
+                    }
+                })
+            }
+        })
+    }, [params.category, params.title])
     
     return (
-        <h1>Something</h1>
+        <figure>
+            <h2>{params.title}</h2>
+            <img src={photo.imageURL} alt={photo.title} />
+        </figure>
     )
 }
